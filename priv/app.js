4912a03427d1591c766baa9db69e6de2900a0cb3
jackdevil <dg@allsol.ru>
2014-03-02 19:25:56 +0800

    Add delay button, fix readme

--- priv/app.js
+++ priv/app.js
@@ -9,8 +9,14 @@ app.controller('MainCtrl', ['$scope', '$rpc',  function($scope, $rpc) {
   $scope.second = 0;
   $scope.result = 0;
 
-  $scope.send = function(){
+  $scope.add = function(){
     $rpc.send('add', parseInt($scope.first), parseInt($scope.second)).then(
+      function(response){console.log('Success', response); $scope.result = response.result},
+      function(response){console.log('Error', response)}
+    );
+  }
+  $scope.add_delay = function(){
+    $rpc.send('add_delay', parseInt($scope.first), parseInt($scope.second)).then(
       function(response){console.log('Success', response); $scope.result = response.result},
       function(response){console.log('Error', response)}
     );
