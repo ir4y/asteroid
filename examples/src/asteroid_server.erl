6aca2f09759c4fb39d28e1bac53bf67116c2d777
Ilya Beda <ir4y.ix@gmail.com>
2014-06-04 12:39:51 +0800

    Refactor project sructure

--- examples/src/asteroid_server.erl
+++ examples/src/asteroid_server.erl
@@ -1,15 +1,29 @@
--module(asteroid_app).
+-module(asteroid_server).
 
--behaviour(application).
+-export([
+    start/0,
+    stop/0
+    ]).
 
-%% Application callbacks
--export([start/2, stop/1]).
+start() ->
+    ok = application:start(crypto),
+    ok = application:start(ranch),
+    ok = application:start(cowlib),
+    ok = application:start(cowboy),
+    ok = application:start(celery),
+    start_cowboy(),
+    ok = sync:go().
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+stop() ->
+    ok = sync:stop(),
+    ok = application:stop(asteroid),
+    ok = application:stop(celery),
+    ok = application:stop(cowboy),
+    ok = application:stop(cowlib),
+    ok = application:stop(ranch),
+    ok = application:stop(crypto).
 
-start(_StartType, _StartArgs) ->
+start_cowboy() ->
     RpcHandlers = dict:from_list([{celery, asteroid_celery},
                                   {pubsub, asteroid_pubsub},
                                   {undefined, asteroid_default}]),
@@ -24,5 +38,3 @@ start(_StartType, _StartArgs) ->
             http, 100, [{ip, Ip}, {port, Port}], [{env, [{dispatch, Dispatch}]}]),
     asteroid_sup:start_link(),
     subscriber:start_link().
-stop(_State) ->
-    ok.
